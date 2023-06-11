0e5f9d0b1e4088f54b530d025cede2f4354aa47c
Alexandr <Sanya_1997_mai@mail.ru>
2023-06-11 15:35:24 +0300

    добавили решение

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задание1
+
+Console.WriteLine("Введите два числа");
+Console.WriteLine("Первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2)
+    {
+        Console.WriteLine("Оба числа одинаковые: " + num1);
+    }
+else if (num1 > num2)
+    {
+        Console.WriteLine("Максимальное число: " + num1);
+    }
+else if (num1 < num2)
+    {
+        Console.WriteLine("Максимальное число: " + num2);
+    }
+
+
+
+
